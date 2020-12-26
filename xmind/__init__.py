09b33d13c17e4696438758b6de830f6f7d2cc445
xmm <xiongmengmeng@qipeipu.com>
2020-12-26 16:52:35 +0800

    1

--- xmind/__init__.py
+++ xmind/__init__.py
@@ -35,3 +35,61 @@ def save(workbook, path=None):
     saver = WorkbookSaver(workbook)
     saver.save(path)
 
+
+def build(content,r2):
+    for key in content:
+        t1 = r2.addSubTopic()
+        t1.setTitle(key)
+        for i in content[key]:
+            if(type(i).__name__=='dict'):
+                for t in i:
+                    t2 = t1.addSubTopic()
+                    t2.setTitle(t)
+                    for j in i[t]:
+                        if(type(j).__name__=='dict'):
+                            for h in j:
+                                t3 = t2.addSubTopic()
+                                t3.setTitle(h) 
+                                for m in j[h]:
+                                    if(type(m).__name__=='dict'):
+                                        for n in m:
+                                            t4 = t3.addSubTopic()
+                                            t4.setTitle(n) 
+                                            for l in m[n]:
+                                                if(type(l).__name__=='dict'):
+                                                    for k in l:
+                                                        t5 = t4.addSubTopic()       
+                                                        t5.setTitle(k)
+                                                        for p in l[k]:
+                                                            if(type(p).__name__=='dict'):
+                                                                for u in p:
+                                                                    t6 = t5.addSubTopic()
+                                                                    t6.setTitle(u)
+                                                                    for y in p[u]:
+                                                                        if(type(y).__name__=='dict'):
+                                                                            for a in y:
+                                                                                t7 = t6.addSubTopic()
+                                                                                t7.setTitle(a)
+                                                                                for b in y[a]:
+                                                                                    t8 = t7.addSubTopic()
+                                                                                    t8.setTitle(b)
+                                                                        else:
+                                                                            t7 = t6.addSubTopic()
+                                                                            t7.setTitle(y)              
+                                                            else:
+                                                                t6 = t5.addSubTopic()
+                                                                t6.setTitle(p)                                                        
+                                                else:
+                                                    t5 = t4.addSubTopic()
+                                                    t5.setTitle(l) 
+                                    else:
+                                        t4 = t3.addSubTopic() 
+                                        t4.setTitle(m) 
+                        else:
+                            t3 = t2.addSubTopic()
+                            t3.setTitle(j) 
+            else:
+                t2 = t1.addSubTopic()
+                t2.setTitle(i) 
+     
+
